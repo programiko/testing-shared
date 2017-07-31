4760a85d9ff7fee3b16547591a66413a76515927
nenadcue <shadowue@hotmail.com>
2017-07-31 22:27:18 +0200

    Staging up a git task

---
+++ GitTaskProject/src/gittaskproject/Example.java
@@ -0,0 +1,55 @@
+/*
+ Svi znaju igru kaladont. Svako kaže reč koja počinje sa zadnja dva slova prethodne reči.
+Zadatak je da svako napiše metod koji će da štampa svoju reč na prethodnu.
+Naravno da ne možete svi odjednom da implementirate svoj metod već jedan po jedan redom kako su poređani metodi da biste znali 
+koju reč vaš emtod da vraća.
+ */
+package gittaskproject;
+import java.lang.Exception;
+
+
+public class Example {
+    public static class Kaladont {
+        
+        public static String InitialWord() {
+            return "Poruka";
+        }
+        
+        public static String Nenad(){
+            throw new RuntimeException();
+        }
+        
+        public static String Dijana(){
+            throw new RuntimeException();
+        }
+        
+        public static String Ana() {
+            throw new RuntimeException();
+        }
+        
+        public static String Ivan() {
+            throw new RuntimeException();
+        }
+        
+        public static String Radenko() {
+            throw new RuntimeException();
+        }
+        
+        public static String Dusko() {
+            throw new RuntimeException();
+        }
+        
+        public static String Marko() {
+            throw new RuntimeException();
+        }
+        
+        public static String Milan() {
+            throw new RuntimeException();
+        }
+        
+        public static String ConcatenateAll(){
+            return InitialWord() + " " + Nenad() + " " + Dijana() + " " + Ana() + " " + Ivan() + " " + Radenko() + " " + Dusko() + " " + Marko () + " " + Milan();
+        }
+                
+    }
+}
---
+++ GitTaskProject/src/gittaskproject/GitTaskProject.java
@@ -0,0 +1,14 @@
+package gittaskproject;
+
+public class GitTaskProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Kaladont resenje:");
+        
+        System.out.println (Example.Kaladont.ConcatenateAll());
+    }
+    
+}
